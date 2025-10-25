cb20fcb97c6a348af7f3404bc07375374702ad8b
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2025-10-25 14:50:59 +0100

    2022 day 2 and refactoring day 1

--- 2022/day1.scala
+++ 2022/day1.scala
@@ -1,13 +1,13 @@
 //> using toolkit default
 
 @main
-def solution(): Unit = {
-  part1()
-  part2()
+def day1(): Unit = {
+  val path = os.pwd / "day1Input.txt"
+  solution(path, false)
+  solution(path, true)
 }
 
-def part1(): Unit = {
-  val path = os.pwd / "day1Input.txt"
+def solution(path: os.Path, part2: Boolean): Unit = {
   val lines = os.read.lines(path)
   var elves: List[Int] = List()
   var cumul = 0
@@ -22,24 +22,8 @@ def part1(): Unit = {
     }
   }
   elves = cumul :: elves
-  println(elves.sortWith((x, y) => x > y).head)
-}
-
-def part2(): Unit = {
-  val path = os.pwd / "day1Input.txt"
-  val lines = os.read.lines(path)
-  var elves: List[Int] = List()
-  var cumul = 0
-  lines.foreach {
-    (line) => {
-      if line == "" then {
-        elves = cumul :: elves
-        cumul = 0
-      } else {
-        cumul += line.toInt
-      }
-    }
-  }
-  elves = cumul :: elves
-  println(elves.sortWith((x, y) => x > y).take(3).sum)
+  if part2 then
+    println(elves.sortWith((x, y) => x > y).take(3).sum)
+  else
+    println(elves.sortWith((x, y) => x > y).head)
 }
---
+++ 2022/day2.scala
@@ -0,0 +1,23 @@
+//> using toolkit default
+
+@main
+def day2(): Unit = {
+  val path = os.pwd / "day2Input.txt"
+  solution(path, false)
+  solution(path, true)
+}
+
+def solution(path: os.Path, part2: Boolean): Unit = {
+  val part1Values = Map("A X" -> 4, "A Y" -> 8, "A Z" -> 3, "B X" -> 1, "B Y" -> 5, "B Z" -> 9, "C X" -> 7, "C Y" -> 2, "C Z" -> 6)
+  val part2Values = Map("A X" -> 3, "A Y" -> 4, "A Z" -> 8, "B X" -> 1, "B Y" -> 5, "B Z" -> 9, "C X" -> 2, "C Y" -> 6, "C Z" -> 7)
+  val lines = os.read.lines(path)
+  var total = 0
+  lines.foreach {
+    (line) =>
+      if part2 then
+        total += part2Values.get(line).getOrElse(0)
+      else
+        total += part1Values.get(line).getOrElse(0)
+  }
+  println(total)
+}
